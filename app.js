423040ba875c95c7c666a402f3afdf7c838d4a0e
YuanZheng Hu <bhu078@uottawa.ca>
2019-10-27 23:53:28 -0400

    test g.mp3

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ var params = {
 
  const url = s3.getSignedUrl('getObject', {
   Bucket: process.env.S3_BUCKET_NAME||'yuanzhenghu',
-  Key:  'Dota 2 Screenshot 2018.04.28 - 17.38.56.14.png',
+  Key:  'g.mp4',
   Expires: 60 * 5
 })
 
