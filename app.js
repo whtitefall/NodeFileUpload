8f9b2f46d88d07d69217f894e21d62daa4c123d9
YuanZheng Hu <bhu078@uottawa.ca>
2019-11-24 22:46:11 -0500

    change url

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ var params = {
 
  const url = s3.getSignedUrl('getObject', {
   Bucket: process.env.S3_BUCKET_NAME||'yuanzhenghu',
-  Key:  'g.mp4',
+  Key:  'Dota 2 Screenshot 2018.01.10 - 19.27.10.21.png',
   Expires: 60 * 5
 })
 
